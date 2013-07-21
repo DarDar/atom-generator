8a49a177bb1cc9e6c109413889e2292277f5094a
Ildar Ibragimov <dar.quonb@gmail.com>
2013-07-21 21:24:14 +0200

    Add http://kuroi.raws.ws/posts/ parser

---
+++ atom_generator/kuroi_raws_ws.py
@@ -0,0 +1,50 @@
+from atom_generator import AtomGeneratorBase
+from lxml import html
+import urllib2
+import json
+import re
+
+
+class AtomGenerator(AtomGeneratorBase):
+
+    def update(self):
+        if not self.src:
+            raise ValueError("Source is not set")
+
+        self._fg.id(self.src)
+        self._fg.link(href=self.src)
+        self._fg.language("en")
+
+        page = urllib2.urlopen(self.src).read()
+        re.S = True
+        info = re.search(r'<script>\s*var posts = \[(.*)\];\s*</script>', page)
+        info = json.loads(info.group(1))
+
+        self._fg.title(info.get("title", self.src))
+
+        author = info.get("user")
+        if author:
+            author = author.get(
+                "fullname",
+                author.get("username"))
+
+        self._fg.pubDate(info.get("createdAt"))
+        self._fg.updated(info.get("updatedAt"))
+
+        div = html.fromstring(info.get("body"))
+        for torrent in div.xpath("//strong[text()='Download']/"
+                                 "following::a[not(text()='magnet')]"):
+            fe = self._fg.add_entry()
+
+            fe.id(torrent.get("href"))
+            fe.title(torrent.text_content())
+            fe.link(href=fe.id())
+
+            if author:
+                fe.author(name=author)
+
+            fe.updated(self._fg.updated())
+
+            fe.content(fe.title())
+
+        return self._fg.atom_str(pretty=True)
--- wsgi-app.py
+++ wsgi-app.py
@@ -3,7 +3,8 @@ from flask import Flask, Response
 from atom_generator import\
     atom_generator,\
     nya_sh,\
-    yousei_raws_org
+    yousei_raws_org,\
+    kuroi_raws_ws
 
 app = Flask(__name__)
 
@@ -31,6 +32,16 @@ def nya_sh_feed(sub=""):
 def yousei_raws_org_feed(sub=""):
     try:
         feed = yousei_raws_org.AtomGenerator("http://yousei-raws.org/%s" % sub)
+    except IOError as e:
+        return Response(atom_generator.error_xml(e), content_type="text/xml; charset=UTF-8")
+
+    return Response(feed.feed(), content_type="text/xml; charset=UTF-8")
+
+
+@app.route("%s/kuroi.raws.ws/<path:sub>" % APPLICATION_ROOT)
+def kuroi_raws_ws_feed(sub=""):
+    try:
+        feed = kuroi_raws_ws.AtomGenerator("http://kuroi.raws.ws/%s" % sub)
     except IOError as e:
         return Response(atom_generator.error_xml(e), content_type="text/xml; charset=UTF-8")
 
