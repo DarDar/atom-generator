bab6d43acf4c4792dfe7fa7aa9717f24d6e18de1
Ildar Ibragimov <dar.quonb@gmail.com>
2013-10-23 23:04:19 +0200

    Move views inside of package

---
+++ atom_generator/__init__.py
@@ -0,0 +1,68 @@
+from flask import Flask
+from feedgen.feed import FeedGenerator
+from lxml import etree
+import urllib2
+import hashlib
+
+
+def error_xml(e):
+    error = etree.Element("error")
+    error.text = str(e)
+    return etree.tostring(error, pretty_print=True, xml_declaration=True, encoding="utf-8")
+
+
+class AtomGeneratorBase:
+
+    # Redefine FeedGenerator.atom_str to return utf-8 string
+    class _FeedGenerator(FeedGenerator):
+        def atom_str(self, pretty=False, extensions=True):
+            feed, doc = self._create_atom(extensions=extensions)
+            return etree.tostring(feed, pretty_print=pretty, xml_declaration=True, encoding="utf-8")
+
+    def _hash(self, str):
+        return hashlib.sha256(str).hexdigest()
+
+    def _src_key(self):
+        return self.__module__ + ":source:" + self.src
+
+    def _xml_key(self):
+        return self.__module__ + ":xml:" + self.src
+
+    def __init__(self, src=None, cache=None):
+        self.src = src
+        self.cache = cache
+        self._fg = self._FeedGenerator()
+        if src:
+            self.update()
+
+    def update(self, force=False):
+        try:
+            if not self.cache:
+                self._xml = self._update()
+            else:
+                page = urllib2.urlopen(self.src).read()
+                src_hash = self._hash(page)
+
+                if not force and self.cache.get(self._src_key()) == src_hash:
+                    self._xml = self.cache.get(self._xml_key())
+                    if self._xml is not None:
+                        return self._xml
+                self._xml = self._update(page)
+
+                with self.cache.pipeline() as pipe:
+                    pipe.set(self._src_key(), src_hash)
+                    pipe.set(self._xml_key(), self._fg.atom_str(pretty=True))
+                    pipe.execute()
+
+        except ValueError as e:
+            self._xml = error_xml(e)
+
+        return self._xml
+
+    def feed(self):
+        return self._xml
+
+app = Flask(__name__)
+app.config.from_object("local_settings")
+
+import atom_generator.views
--- wsgi-app.py
+++ wsgi-app.py
@@ -1,53 +1,5 @@
 #!/usr/bin/env python
-from flask import Flask, Response
-from atom_generator import (
-    atom_generator,
-    nya_sh,
-    yousei_raws_org,
-    kuroi_raws_ws,
-)
-from redis import StrictRedis
-
-app = Flask(__name__)
-
-app.config.from_object("local_settings")
-
-if app.config["REDIS"]:
-    cache = StrictRedis(**app.config["REDIS"])
-else:
-    cache = None
-
-
-@app.route("/nya.sh")
-@app.route("/nya.sh/<sub>")
-def nya_sh_feed(sub=""):
-    try:
-        feed = nya_sh.AtomGenerator("http://nya.sh/%s" % sub, cache)
-    except IOError as e:
-        return Response(atom_generator.error_xml(e), content_type="text/xml; charset=UTF-8")
-
-    return Response(feed.feed(), content_type="text/xml; charset=UTF-8")
-
-
-@app.route("/yousei-raws.org/<path:sub>")
-def yousei_raws_org_feed(sub=""):
-    try:
-        feed = yousei_raws_org.AtomGenerator("http://yousei-raws.org/%s" % sub, cache)
-    except IOError as e:
-        return Response(atom_generator.error_xml(e), content_type="text/xml; charset=UTF-8")
-
-    return Response(feed.feed(), content_type="text/xml; charset=UTF-8")
-
-
-@app.route("/kuroi.raws.ws/<path:sub>")
-def kuroi_raws_ws_feed(sub=""):
-    try:
-        feed = kuroi_raws_ws.AtomGenerator("http://kuroi.raws.ws/%s" % sub, cache)
-    except IOError as e:
-        return Response(atom_generator.error_xml(e), content_type="text/xml; charset=UTF-8")
-
-    return Response(feed.feed(), content_type="text/xml; charset=UTF-8")
-
+from atom_generator import app as application
 
 if __name__ == "__main__":
-    app.run()
+    application.run()
