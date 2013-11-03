89cc5064152f1d04e9960e79b606efaf13b97ea1
Ildar Ibragimov <dar.quonb@gmail.com>
2013-11-03 20:57:11 +0100

    Add YouTube video info extractor

--- atom_generator/__init__.py
+++ atom_generator/__init__.py
@@ -3,12 +3,68 @@ from feedgen.feed import FeedGenerator
 from lxml import etree
 import urllib2
 import hashlib
+import re
 
 
 def error_xml(e):
     error = etree.Element("error")
     error.text = str(e)
     return etree.tostring(error, pretty_print=True, xml_declaration=True, encoding="utf-8")
+
+
+class YouTube(object):
+    def __init__(self, url=None):
+        self._video_id = self._extract_id(url)
+
+    @property
+    def video_id(self):
+        return self._video_id
+
+    @video_id.setter
+    def video_id(self, url):
+        self._video_id = self._extract_id(url)
+
+    def _extract_id(self, url=None):
+        """Extract youtube video ID
+
+        Based on `youtube_dl` code
+        """
+        if not url:
+            return None
+
+        YOUTUBE_URL = r"""^
+(?:
+    (?:https?://)?                      # http(s):// (optional)
+    (?:(?:(?:
+        (?:\w+\.)?youtube(?:-nocookie)?\.com/|
+        tube\.majestyc\.net/|
+        youtube\.googleapis\.com/)      # the various hostnames, with wildcard subdomains
+    (?:.*?\#/)?                         # handle anchor (#/) redirect urls
+    (?:                                 # the various things that can precede the ID:
+        (?:(?:v|embed|e)/)|             # v/ or embed/ or e/
+        (?:                             # or the v= param in all its forms
+            (?:
+                (?:watch|movie)(?:_popup)?(?:\.php)?
+            )?                          # preceding watch(_popup|.php) or nothing (like /?v=xxxx)
+            (?:\?|\#!?)                 # the params delimiter ? or # or #!
+            (?:.*?&)?                   # any other preceding param (like /?s=tuff&v=xxxx)
+            v=
+        )
+    ))|
+    youtu\.be/                          # just youtu.be/xxxx
+    )
+)?                                      # all until now is optional -> you can pass the naked ID
+([0-9A-Za-z_-]{11})                     # here is it! the YouTube video ID
+(?(1).+)?                               # if we found the ID, everything can follow
+$"""
+        video_id = re.match(YOUTUBE_URL, str(url), re.VERBOSE)
+        return video_id and video_id.group(1)
+
+    def thumbnail(self):
+        return self.video_id and "http://i.ytimg.com/vi/%s/0.jpg" % self.video_id
+
+    def video(self):
+        return self.video_id and "http://www.youtube.com/watch?v=%s" % self.video_id
 
 
 class AtomGeneratorBase:
